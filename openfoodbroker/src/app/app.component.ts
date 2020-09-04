ed337d594b03e814565da574c5d3b05c448c9279
developerajay <malhotraajay78@yahoo.com>
2020-09-04 11:39:20 +0530

    datatable and autocomplete implementation

--- openfoodbroker/src/app/app.component.ts
+++ openfoodbroker/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openfoodbroker';
+   
+  dtOptions: any = {};
+  showtable:boolean;
+  showdata:any
+  exampleData = [
+    {
+      id: 1,
+      name: 'Dil'
+    },
+    {
+      id: 2,
+      name: 'Dattel'
+    },
+    {
+      id: 3,
+      name: 'Klee'
+    },
+    {
+      id: 4,
+      name: 'Akee'
+    }
+  ];
+  keyword = 'name';
+  ngOnInit()
+  {
+    
+      
+       this.showtable = false;
+       
+       this.dtOptions = {
+        pagingType: 'full_numbers',
+        pageLength: 5,
+        lengthMenu : [5, 10, 25],
+        processing: true,
+        searching: false, 
+        paging: false, 
+        info: false
+
+      };
+      
+      
+       
+  }
+  public selectEvent(e: any): void {
+  
+    this.showtable = true;
+  }
+
+  public getServerResponse(e: any): void {
+  
+   this.showdata = this.exampleData;
+  }
+
+  searchCleared() {
+    
+    this.showdata = [];
+    //this.showtable = false;
+  }
+
+  
 }
--- openfoodbroker/src/app/app.module.ts
+++ openfoodbroker/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DataTablesModule} from 'angular-datatables';
+
+import { NgSelect2Module } from 'ng-select2';
+
+import {AutocompleteLibModule} from 'angular-ng-autocomplete';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    NgSelect2Module,
+    AutocompleteLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
